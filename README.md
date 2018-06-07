aec0999c60bf5cf5fe25b9f7d94fdc48d246f2a7
anilnanda <anilnandat@gmail.com>
2018-06-07 07:06:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains all the common steps executed for building a docker image
---
+++ src/common/script.groovy
@@ -0,0 +1,49 @@
+import net.sf.json.JSONObject
+import net.sf.json.JSONArray
+
+def random
+
+
+def getRandom(){
+if (random){
+	return random
+} else{
+	random= new Random()
+	return random
+}
+}
+
+def createDir(dirName){
+	sh "mkdir -p ${dirName}"
+}
+
+
+def setAWSRegion(region) {
+
+sh "aws configure set region $(region)"
+}
+
+def cleanWs(){
+	echo "Clean workspace at ${pwd()}"
+	sh "sudo rm -rf ${pwd()}/*"
+}
+
+
+def cleanUpDockerImages() {
+	echo "Clean un-used docker images"
+	sh "sudo docker system prune -af"
+}
+
+def checkoutGitFromScm(scm){
+	checkout changelog: true poll: true
+	scm: [
+		$class: 'GitSCM',
+		branches: scm.branches,            
+		doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,            
+		extensions: scm.extensions,
+            userRemoteConfigs: scm.userRemoteConfigs
+        ]
+}
+
+
+
